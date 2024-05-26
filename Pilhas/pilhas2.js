fe9e5e63d7d4786a354d3d8cfd2299ed97971773
tlshudson <hudsonteles00@gmail.com>
2024-05-26 18:17:25 -0300

    +1 exercicio sobre pilha

---
+++ Pilhas/pilhas2.js
@@ -0,0 +1,12 @@
+//código descritivo com mais métodos de pilha
+const stack = {
+    itens: [], //contém todos os elementos da pilha
+
+    this:push() = function(elemento){
+        //adiciona um elemento ao fim da pilha, o qual vai ser retirado primeiro
+    },
+
+    this:pop = function(){
+        //retira o último elemento da pilha
+    }
+}; 
